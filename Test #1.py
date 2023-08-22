a108338a4778feffc67f25593df9bb652244c354
J. Ward <60374778+XxJaytheBeastX1@users.noreply.github.com>
2023-08-22 16:24:06 -0500

    Add files via upload
    Just import by pressing the folders near the start icons

---
+++ Test #1.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+while not at_goal():
+    if front_is_clear() and wall_on_right():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
+    else:
+        turn_left()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
